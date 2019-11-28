1575283987370b966fe8edf55144a242ba1a74fb
Ståle Gjelsten <stalegjelsten@gmail.com>
2019-11-28 08:16:29 +0100

    Fixed bug where eventID was hard coded for each entry

--- pyeventor2ttime.py
+++ pyeventor2ttime.py
@@ -6,6 +6,7 @@ def read_excel_xml(path):
     file = open(path,'r',encoding="utf-8").read()
     soup = BeautifulSoup(file,'xml')
     workbook = []
+    print("Reading data from .xls file")
     for sheet in soup.findAll('Table'): 
         sheet_as_list = []
         for row in sheet.findAll('Row'):
@@ -19,10 +20,11 @@ def read_excel_xml(path):
 data = read_excel_xml(sys.argv[1])
 filename = sys.argv[1].split()
 eventID = filename[2].split('.')[0]
+print("Converting data and writing .csv file")
 df = pd.DataFrame(data[7][1:],columns = data[7][0])
 df["Navn"] = df["Fornavn"] + " " + df["Etternavn"]
 df["Status"] = "x"
-df["Spesial"] = "A,11794,18," + df["Organisasjons-id"] + "," + df["Person-id"]
+df["Spesial"] = "A," + eventID + ",18," + df["Organisasjons-id"] + "," + df["Person-id"]
 df = df[["Person-id","Status","Navn","Klasse","Klubb","Spesial","Emit"]]
 outfile = "EntryDB " + str(eventID) + ".csv"
 df.to_csv(outfile,sep=";",header=False,index=False,encoding="iso-8859-1")
